ff85e9440bcd8bcae9129a997c842d756af2134f
sgraziola <simo.fdl@gmail.com>
2022-10-14 12:39:32 +0200

    Ex palindroma: Add program without function

--- ex_palindroma/assets/js/main.js
+++ ex_palindroma/assets/js/main.js
@@ -3,9 +3,28 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
  */
 
 //Chiedere all’utente di inserire una parola con prompt
+const userWord = prompt("Inserisci una parola e ti dirò se è palindroma");
 //creare funzione palindroma
+
     //split() converto string di prompt in array
     //creo nuova const
     //-reverse() invertire ordine elementi array
-    // Se primo array uguale al secondo array allora la parola inserita è palindroma
+    //uso join per trasformare il nuovo array in una seconda stringa
+    // Se la nuova stringa è uguale alla parola inserita dall'utente allora la parola inserita è palindroma
 
+
+const firstArray = userWord.split("");
+console.log(firstArray);
+const secondArray = firstArray.reverse();
+console.log(secondArray);
+const newWord = secondArray.join("");
+let isPalindromic = false;
+if (newWord === userWord){
+    isPalindromic = true;
+}
+if (isPalindromic){
+    console.log(`${userWord} è una parola palindroma`)
+} else {
+    console.log(`${userWord} non è una parola palindroma`)
+
+}
