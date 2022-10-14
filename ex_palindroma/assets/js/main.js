f0411eadfcb5b77e43d7629c2d44676c1dde4501
sgraziola <simo.fdl@gmail.com>
2022-10-14 13:04:10 +0200

    Ex palindroma: Add results in page

--- ex_palindroma/assets/js/main.js
+++ ex_palindroma/assets/js/main.js
@@ -5,30 +5,12 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 //Chiedere all’utente di inserire una parola con prompt
 const userWord = prompt("Inserisci una parola e ti dirò se è palindroma");
 //creare funzione palindroma
-
     //split() converto string di prompt in array
     //creo nuova const
     //-reverse() invertire ordine elementi array
     //uso join per trasformare il nuovo array in una seconda stringa
     // Se la nuova stringa è uguale alla parola inserita dall'utente allora la parola inserita è palindroma
 
-/* 
-const firstArray = userWord.split("");
-console.log(firstArray);
-const secondArray = firstArray.reverse();
-console.log(secondArray);
-const newWord = secondArray.join("");
-let isPalindromic = false;
-if (newWord === userWord){
-    isPalindromic = true;
-}
-if (isPalindromic){
-    console.log(`${userWord} è una parola palindroma`)
-} else {
-    console.log(`${userWord} non è una parola palindroma`)
-
-}
- */
 function palindroma(word) {
 const firstArray = word.split("");
 const secondArray = firstArray.reverse();
@@ -41,9 +23,9 @@ return isPalindromic;
 }
 
 palindroma(userWord);
+const verifiedEl = document.querySelector("h1");
 if (palindroma(userWord)){
-    console.log(`${userWord} è una parola palindroma`);
+    verifiedEl.innerHTML= (`${userWord} è una parola palindroma`);
 } else {
-    console.log(`${userWord} non è una parola palindroma`);
-
+    verifiedEl.innerHTML= (`${userWord} non è una parola palindroma`);
 }
