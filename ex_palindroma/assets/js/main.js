af2971d7fa50547f81f9a851a7426743a878976a
sgraziola <simo.fdl@gmail.com>
2022-10-14 12:18:55 +0200

    Ex palidroma: Add comments to what to do

---
+++ ex_palindroma/assets/js/main.js
@@ -0,0 +1,11 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+ */
+
+//Chiedere all’utente di inserire una parola con prompt
+//creare funzione palindroma
+    //split() converto string di prompt in array
+    //creo nuova const
+    //-reverse() invertire ordine elementi array
+    // Se primo array uguale al secondo array allora la parola inserita è palindroma
+
